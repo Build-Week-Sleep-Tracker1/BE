f4280245eaf7273e63200918ef29d419c09638c9
RyanPlanteNJ <ryanm.plante@gmail.com>
2020-08-19 18:45:05 -0400

    fixing sever and index files

--- api/server.js
+++ api/server.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const cors = require('cors');
-const helmet = require('hemlet');
+const helmet = require('helmet');
 const morgan = require('morgan');
 
-const authenticate = rquire('../auth/authenticate-middleware.js');
-const authRouter = require('../auth/auth-router.js');
-const usersRouter = require('../user/user-router.js');
+// const authenticate = require('../auth/authenticate-middleware.js');
+// const authRouter = require('../auth/auth-router.js');
+// const usersRouter = require('../user/user-router.js');
 
-const server = exppress();
+const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
 server.use(morgan('dev'));
 
-server.use('/api/auth', authRouter);
-server.use('/api/users', authenticate, usersRouter)
+// server.use('/api/auth', authRouter);
+// server.use('/api/users', authenticate, usersRouter)
 
 server.get("/", (req, res) => {
   res.status(201).json({api: "Time to get some sleep"})
