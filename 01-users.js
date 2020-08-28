9bdddbf5cd41b712e13be537d53838bdf19e6829
RyanPlanteNJ <ryanm.plante@gmail.com>
2020-08-28 00:09:11 -0400

    fixing seed issues

---
+++ 01-users.js
@@ -0,0 +1,8 @@
+// exports.seed = async function (knex) {
+//   await knex('users').insert([
+//     { username: "Chortle", password: "theking" },
+//     { username: "frodo", password: "ringbearer" },
+//     { username: "sam", password: "kingcrab" },
+//     { username: "pippin", password: "theidiot" }
+//   ])
+// }
